6ffae0b22073b90898382ef70faf14d422b18639
Sanjay Saju Jacob <sanjaysajujacob@gmail.com>
2022-04-19 12:48:52 +0530

    Added main page template

---
+++ templates/js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+    var roles = ["All Brands", "Free Support", "Affordable designs"];
+    var count = 0;
+    var $typeSpan = $("#intro .content h2 span").typist({
+        text: roles[count]
+    });
+    setInterval(() => {
+        $typeSpan
+            .typistRemove(roles[count++ % roles.length].length)
+            .typistPause(2000)
+            .typistAdd(roles[count % roles.length]);
+    }, 5000);
+    $(".navbar").hide();
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 40) {
+            $(".navbar").slideDown(100);
+        } else {
+            $(".navbar").slideUp(100);
+        }
+    });
+    $(".go-down a").click(function(e) {
+        e.preventDefault();
+        var top = $($(this).attr("href")).position().top - 70;
+        $("html, body").animate({
+                scrollTop: top
+            },
+            1000
+        );
+    });
+});
